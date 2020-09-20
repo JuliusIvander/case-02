ca1b3917bcae386fab883c5018c3b4a74662de53
Julius Ivander <50763563+JuliusIvander@users.noreply.github.com>
2020-09-20 20:13:46 +0700

    Update user.go

--- entity/user.go
+++ entity/user.go
@@ -3,7 +3,7 @@ package entity
 type (
 	//User entity
 	User struct {
-		ID        string `json:"id"`
+		ID        string `json:"id,omitempty"`
 		Nama      string `json:"nama"`
 		Email     string `json:"email"`
 		NomorHp   string `json:"nomorhp"`
